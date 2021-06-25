b9fad0c4121bb8b51b0e1b7e1383f2fa1d857f0e
Rika <40230165+Moreonenight@users.noreply.github.com>
2021-06-25 12:22:52 +0800

    Test Migration

--- DotnetBookCloud/Migrations/20210625042009_Before_Final.Designer.cs
+++ DotnetBookCloud/Migrations/20210625042009_Before_Final.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DotnetBookCloud;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DotnetBookCloud.Migrations
 {
     [DbContext(typeof(BookCloudDBContext))]
-    partial class BookCloudDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210625042009_Before_Final")]
+    partial class Before_Final
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotnetBookCloud/Migrations/20210625042009_Before_Final.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetBookCloud.Migrations
+{
+    public partial class Before_Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
